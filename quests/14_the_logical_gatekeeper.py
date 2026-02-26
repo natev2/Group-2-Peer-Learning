0d534976dda704f8c2b1537318685ff910b75fe9
mmuuse-dot <m.muuse@alustudent.com>
2026-02-26 14:16:38 -0800

    14_the_logical_gatekeeper.py

---
+++ quests/14_the_logical_gatekeeper.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+gold = int(input("Enter the amount of gold coins you have: "))
+
+if age >= 18 and gold >= 20:
+    print("You may enter the club!")
+else:
+    print("Access denied. You do not meet the requirements.")
