8e9f3f58a0f3558050ed87a7959ed1c0115e6aac
mtndlovu <mtndlovu81@gmail.com>
2026-02-28 02:10:25 +0200

    Completed quests

---
+++ quests/quest_12.py
@@ -0,0 +1,7 @@
+# password checker
+password = "secret"
+user_input = input("Enter your password: ")
+if user_input == password:
+    print("Access Granted")
+else:
+    print("Access Denied")
--- quests/quest_30.py
+++ quests/quest_30.py
@@ -1,11 +1,13 @@
 def ask_for_age():
-    age = int(input("What is your age? "))
+    # function takes user input and returns integer
+    age = int(input("What is your age? "))  # prompts user for input and converts it to an int
     return age
 
 def can_they_vote(age):
+    # check if user is eligible to vote
     if age >= 18:
         print("You can vote.")
     else:
         print("You are not eligible to vote.")
 
-can_they_vote(ask_for_age())
+can_they_vote(ask_for_age())  # use the result of calling the function ask_for_age() as an argument
